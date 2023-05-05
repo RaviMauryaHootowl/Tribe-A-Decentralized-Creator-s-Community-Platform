3323ca65594ea9930a3c4d73ed84505c6937b4a8
RaviMauryaHootowl <ravimaurya027@gmail.com>
2023-05-05 12:00:24 +0530

    upading contract address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import fund from "../artifacts/contracts/crowd_funding.sol/crowd_funding.json"
 
-export const ContractAddress = "0x28e2c3A6a1B85403e92d74751a4c427927438370";
+export const ContractAddress = "0x03087aE2DACeB2b2758a1877253D635446518db2";
 export const ContractABI = fund.abi;
