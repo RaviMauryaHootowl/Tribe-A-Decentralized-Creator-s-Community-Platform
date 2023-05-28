9c669b7839cb20d1bff89617103cf221bc0759e3
RaviMauryaHootowl <ravimaurya027@gmail.com>
2023-05-28 19:11:57 +0530

    changing ABI path to non gasless

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-import fund from "../artifacts/contracts/crowd_funding_gasless.sol/crowd_funding_gasless.json"
+import fund from "../artifacts/contracts/crowd_funding.sol/crowd_funding.json"
 
 export const ContractAddress = "0x1aa364E32E904B17d1be4B40cf068095aA386127";
 export const ContractABI = fund.abi;
